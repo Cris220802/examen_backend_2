81b8a82aa5a78f479648225972cc323de7652c6c
Cristopher <cris220802@hotmail.com>
2024-11-28 08:52:40 -0600

    third commit

--- src/main/java/com/examen/time/config/SecurityConfig.java
+++ src/main/java/com/examen/time/config/SecurityConfig.java
@@ -29,97 +29,107 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
-    @Autowired
-    private AuthenticationConfiguration authenticationConfiguration;
+        @Autowired
+        private AuthenticationConfiguration authenticationConfiguration;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        // Configuración de la seguridad personalizada
-        return httpSecurity
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(http -> {
-                    http
-                            .requestMatchers("/swagger-ui/**", "/swagger-ui.html").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/v1/time/**").hasAnyAuthority("READ")
-                            .requestMatchers(HttpMethod.POST, "/api/v1/time/**").hasAnyAuthority("CREATE")
-                            .requestMatchers(HttpMethod.PUT, "/api/v1/time/**").hasAnyAuthority("UPDATE")
-                            .requestMatchers(HttpMethod.DELETE, "/api/v1/time/**").hasAnyAuthority("DELETE")
-                            .anyRequest().denyAll();
-                })
-                .build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                // Configuración de la seguridad personalizada
+                return httpSecurity
+                                .csrf(csrf -> csrf
+                                                .ignoringRequestMatchers("/api/v1/time/**") // Ignorar CSRF en rutas
+                                                                                            // específicas
+                                )
+                                .httpBasic(Customizer.withDefaults())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(http -> {
+                                        http
+                                                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html")
+                                                        .permitAll()
+                                                        .requestMatchers(HttpMethod.GET, "/api/v1/time/**")
+                                                        .hasAnyAuthority("READ")
+                                                        .requestMatchers(HttpMethod.POST, "/api/v1/time/**")
+                                                        .hasAnyAuthority("CREATE")
+                                                        .requestMatchers(HttpMethod.PUT, "/api/v1/time/**")
+                                                        .hasAnyAuthority("UPDATE")
+                                                        .requestMatchers(HttpMethod.DELETE, "/api/v1/time/**")
+                                                        .hasAnyAuthority("DELETE")
+                                                        .anyRequest().denyAll();
+                                })
+                                .build();
+        }
 
-    // Authentication Manager
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        // Authentication Manager
+        @Bean
+        public AuthenticationManager authenticationManager() throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 
-    // Autehntication Provider -> Dao -> Va a proporcionar la autenticación
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-        return daoAuthenticationProvider;
-    }
+        // Autehntication Provider -> Dao -> Va a proporcionar la autenticación
+        @Bean
+        public AuthenticationProvider authenticationProvider() {
+                DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+                daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+                daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+                return daoAuthenticationProvider;
+        }
 
-    // Password Encoder
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // return new BCryptPasswordEncoder();
-        return NoOpPasswordEncoder.getInstance();
-    }
+        // Password Encoder
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                // return new BCryptPasswordEncoder();
+                return NoOpPasswordEncoder.getInstance();
+        }
 
-    // UserDetailsService -> base de datos o memoria
-    @Bean
-    public UserDetailsService userDetailsService() {
-        // Definir usuarios en memoria
-        // No vamos a usar una base de datos
-        UserDetails admin = User.withUsername("admin")
-        .password("admin")
-        .roles("ADMIN")
-        .authorities("READ", "CREATE", "UPDATE", "DELETE")
-        .build();
+        // UserDetailsService -> base de datos o memoria
+        @Bean
+        public UserDetailsService userDetailsService() {
+                // Definir usuarios en memoria
+                // No vamos a usar una base de datos
+                UserDetails admin = User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                                .authorities("READ", "CREATE", "UPDATE", "DELETE")
+                                .build();
 
-        UserDetails user = User.withUsername("user")
-                .password("user")
-                .roles("User")
-                .authorities("READ")
-                .build();
+                UserDetails user = User.withUsername("user")
+                                .password("user")
+                                .roles("User")
+                                .authorities("READ")
+                                .build();
 
-        UserDetails moderator = User.withUsername("moderator")
-                .password("moderator")
-                .roles("MODERATOR")
-                .authorities("READ", "UPDATE")
-                .build();
+                UserDetails moderator = User.withUsername("moderator")
+                                .password("moderator")
+                                .roles("MODERATOR")
+                                .authorities("READ", "UPDATE")
+                                .build();
 
-        UserDetails editor = User.withUsername("editor")
-                .password("editor")
-                .roles("EDITOR")
-                .authorities("UPDATE")
-                .build();
+                UserDetails editor = User.withUsername("editor")
+                                .password("editor")
+                                .roles("EDITOR")
+                                .authorities("UPDATE")
+                                .build();
 
-        UserDetails developer = User.withUsername("developer")
-                .password("DEVELOPER")
-                .roles("EDITOR")
-                .authorities("READ", "UPDATE", "CREATE", "DELETE", "WRITE")
-                .build();
+                UserDetails developer = User.withUsername("developer")
+                                .password("developer")
+                                .roles("EDITOR")
+                                .authorities("READ", "UPDATE", "CREATE", "DELETE", "WRITE")
+                                .build();
 
-        UserDetails analyst = User.withUsername("analyst")
-                .password("ANALYST")
-                .roles("EDITOR")
-                .authorities("READ", "DELETE")
-                .build();
+                UserDetails analyst = User.withUsername("analyst")
+                                .password("analyst")
+                                .roles("EDITOR")
+                                .authorities("READ", "DELETE")
+                                .build();
 
-        List<UserDetails> usersDetailList = new ArrayList<>();
-        usersDetailList.add(admin);
-        usersDetailList.add(user);
-        usersDetailList.add(moderator);
-        usersDetailList.add(editor);
-        usersDetailList.add(developer);
-        usersDetailList.add(analyst);
-        return new InMemoryUserDetailsManager(usersDetailList);
-    }
+                List<UserDetails> usersDetailList = new ArrayList<>();
+                usersDetailList.add(admin);
+                usersDetailList.add(user);
+                usersDetailList.add(moderator);
+                usersDetailList.add(editor);
+                usersDetailList.add(developer);
+                usersDetailList.add(analyst);
+                return new InMemoryUserDetailsManager(usersDetailList);
+        }
 }
